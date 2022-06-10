03025aaf3a7a08192f6f4b6ab0449c8bd60d7071
Elena <avolkova72@yandex.ru>
2022-06-10 17:04:49 +0300

    modiefed  files

--- 21_diploma/js/index.js
+++ 21_diploma/js/index.js
@@ -7,28 +7,42 @@ document.addEventListener("DOMContentLoaded", function () {
 
 //in ether
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector(".header__ether-link").addEventListener("click", function () {
+  document
+    .querySelector(".header__ether-link")
+    .addEventListener("click", function () {
       document.querySelector(".header").classList.toggle("is-open");
     });
 });
 
 // search
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector(".header__btn-search").addEventListener("click", function () {
-      document.querySelector(".menu-search__form").classList.toggle("menu-search-open");
+  document
+    .querySelector(".header__btn-search")
+    .addEventListener("click", function () {
+      document
+        .querySelector(".menu-search__form")
+        .classList.toggle("menu-search-open");
     });
+});
+
+//Подключение модального окна
+const modal = new GraphModal();
+
+//Очистка инпутов формы входа по нажатию кнопки "Закрыть"
+
+let btnClear = document.querySelector(".header__btn-closed");
+let inputs = document.querySelectorAll("input");
+
+btnClear.addEventListener("click", () => {
+  inputs.forEach((input) => (input.value = ""));
 });
 
 //validation формы для входа
 
 const validation = new JustValidate(".header__form-enter", {
   errorFieldCssClass: "is-invalid",
-  errorLabelStyle: {
-    fontSize: "12px",
-    color: "#ff6f6f",
-  },
   focusInvalidField: true,
-}); //Меняем стили к сообщению об ошибке
+}); //Здесь были стили к ошибке, теперь они в CSS
 
 validation
   .addField(".header__input-login", [
@@ -38,8 +52,8 @@ validation
     },
     {
       rule: "minLength",
-      value: 2,
-      errorMessage: "Логин не может быть короче 2 знаков",
+      value: 3,
+      errorMessage: "Логин не может быть короче 3 знаков",
     },
     {
       rule: "maxLength",
@@ -82,22 +96,135 @@ btn.forEach(function (i) {
   });
 });
 
+// Кнопка ещё подкасты
 
+$(function () {
+  $(".podcast__btn-bottom").on("click", function () {
+    $(".podcast__item:hidden").slideDown(1000);
+  });
+});
 
+//select
+const element = document.querySelector(".broadcast__select");
+const choices = new Choices(element, {
+  searchEnabled: false, //удаляет окно поиска
+  shouldSort: false, //отключает сортировку по алфавиту
+  itemSelectText: "", //удаляет placeholder
+});
 
+//accordion
 
+$(".guest__accordion-list").accordion({
+  active: 0, //делает открытым 1-й блок
+  heightStyle: "content", //подстраивает ширину открытого окна под контент
+});
+$(".ui-accordion-header").attr("tabindex", "0"); //чтобы работал фокус на Tab
 
+//tabs in accordion
 
+document.querySelectorAll(".guest__name").forEach(function (tabsBtn) {
+  //выбираем все кнопки и для каждой назначаем функуию
+  tabsBtn.addEventListener("click", function (event) {
+    //функция срабатывает по клику, происходит связывание атрибута path с атрибутом target с одним значением.
+    const path = event.currentTarget.dataset.path;
+    document.querySelectorAll(".guest__name").forEach(function (btnAct) {
+      btnAct.classList.remove("presona-active"); //по клику удаляет указанный класс
+    });
+    event.currentTarget.classList.add("presona-active"); //по клику добавляет класс
+    document
+      .querySelectorAll(".guest__persona-item")
+      .forEach(function (tabsBtn) {
+        tabsBtn.classList.remove("presona-active");
+      });
+    document
+      .querySelector(`[data-target="${path}"]`)
+      .classList.add("presona-active");
+  });
+});
 
+//swipper
 
-//код от куратора чата на кнопки play
-// const button = document.querySelectorAll('.btn'); // здесь класс всех кнопок
+const slider = document.querySelector(".swiper");
 
-// button.forEach((el) => {
-//   el.addEventListener('click', (e) => {
-//     button.forEach((item) => {
-//       item.classList.remove('класс');
-//     });
-//     e.currentTarget.classList.add('класс');
-//   });
-// });
+var mySwiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+//validation отправки сообщения
+
+const validation2 = new JustValidate(".about__form", {
+  errorFieldCssClass: "is-invalid",
+  focusInvalidField: true,
+});
+
+validation2
+  .addField(".about__textarea", [
+    {
+      rule: "required",
+      errorMessage: "Введите сообщение",
+    },
+  ])
+  .addField(".about__input-name", [
+    {
+      rule: "required",
+      errorMessage: "Введите ваше имя",
+    },
+    {
+      rule: "minLength",
+      value: 2,
+      errorMessage: "Имя не может быть короче 2 знаков",
+    },
+    {
+      rule: "maxLength",
+      value: 30,
+      errorMessage: "Имя не может быть длиннее 30 знаков",
+    },
+    {
+      rule: "customRegexp",
+      value: /(?=.*[а-яА-ЯёЁa-zA-Z])/,
+      errorMessage: "Ошибка",
+    }, // В имени используются латиница, кириллица
+  ])
+  .addField(".about__input-email", [
+    {
+      rule: "required",
+      errorMessage: "Введите E-mail",
+    },
+    {
+      rule: "email",
+      errorMessage: "Email введён в неправильном формате",
+    },
+  ])
+
+  //Отправка формы на почту
+  .onSuccess((event) => {
+    console.log("Validation passes and form submitted", event);
+
+    let formData = new FormData(event.target);
+
+    console.log(...formData);
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          console.log("Отправлено");
+        }
+      }
+    };
+
+    xhr.open("POST", "mail.php", true);
+    xhr.send(formData);
+
+    event.target.reset();
+  });
+
+//Подключение картинки sending.gif
+  // document.querySelector(".about__form-btn").addEventListener("click", function () {
+  //   document.querySelector(".about__form-wrap").classList.add("sending");    
+  // });
+  
